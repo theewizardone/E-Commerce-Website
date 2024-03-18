2c892c0244c5ebaa6488843ab86a4ae3d07f0ed6
Alfonce Morara <alfoncemorara412@gmail.com>
2024-03-19 01:30:08 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,106 @@
+<footer id="footer" style="background:black;color:white;>
+			<!-- top footer -->
+			<div class="section">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">About Us</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+									sed do eiusmod tempor incididunt ut labore et dolore magna 
+									aliqua. Ut enim ad minim veniam, </p>
+								<ul class="footer-links">
+									<li><a href="#" style="color:blue;"><i class="fa fa-map-marker"></i>Nyarugenge, Kigali</a></li>
+									<li><a href="#" style="color:blue;"><i class="fa fa-phone"></i>+250xxxxxxxxx</a></li>
+									<li><a href="#" style="color:blue;"><i class="fa fa-envelope-o"></i>gmail@gmail.com</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-6 text-center" style="margin-top:80px;">
+							<ul class="footer-payments">
+								<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
+								<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
+							</ul>
+							<span class="copyright">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> Website Designed & Developed By
+<i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank" style="color:blue;">Nshuti Ntigurirwa Albert</a>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</span>
+						</div>
+
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">Categories</h3>
+								<ul class="footer-links">
+									<li><a href="#" style="color:blue;">Mobiles</a></li>
+									<li><a href="#" style="color:blue;">Men</a></li>
+									<li><a href="#" style="color:blue;">Women</a></li>
+									<li><a href="#" style="color:blue;">Kids</a></li>
+									<li><a href="#" style="color:blue;">Accessories</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="clearfix visible-xs"></div>
+
+						
+					</div>
+					<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /top footer -->
+                
+
+			<!-- bottom footer -->
+			
+			<!-- /bottom footer -->
+		</footer>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/actions.js"></script>
+		<script src="js/sweetalert.min"></script>
+		<script src="js/jquery.payform.min.js" charset="utf-8"></script>
+    <script src="js/script.js"></script>
+		<script>var c = 0;
+        function menu(){
+          if(c % 2 == 0) {
+            document.querySelector('.cont_drobpdown_menu').className = "cont_drobpdown_menu active";    
+            document.querySelector('.cont_icon_trg').className = "cont_icon_trg active";    
+            c++; 
+              }else{
+            document.querySelector('.cont_drobpdown_menu').className = "cont_drobpdown_menu disable";        
+            document.querySelector('.cont_icon_trg').className = "cont_icon_trg disable";        
+            c++;
+              }
+        }
+           
+		
+</script>
+    <script type="text/javascript">
+		$('.block2-btn-addcart').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+			});
+		});
+
+		$('.block2-btn-addwishlist').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+			});
+		});
+	</script>
+	
---
+++ newslettter.php
@@ -0,0 +1,41 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href=""><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href=""><img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/twitterx--v2.png" alt="twitterx--v2"/></a>
+								</li>
+								<li>
+									<a href=""><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href=""><img width="20" height="20" src="https://img.icons8.com/sf-black-filled/64/youtube-play.png" alt="youtube-play"/></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
